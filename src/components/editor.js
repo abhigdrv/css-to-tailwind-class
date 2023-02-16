6e3d1e528778a6fe971f8a3f560a74f37ac77f5a
Abhishek Vishwakarma <52538014+abhigdrv@users.noreply.github.com>
2023-02-16 19:31:54 +0530

    fix: final data on screen

--- src/components/editor.js
+++ src/components/editor.js
@@ -1,94 +1,115 @@
 // https://github.com/scniro/react-codemirror2/issues/83
-import React, { useState, useEffect } from 'react';
-import { Controlled as CodeMirror } from 'react-codemirror2';
-import 'codemirror/addon/edit/matchbrackets';
-import 'codemirror/addon/edit/closebrackets';
-import 'codemirror/addon/comment/comment';
-import 'codemirror/addon/lint/lint.css';
-import 'codemirror/addon/search/match-highlighter';
-import parseCSS from 'css-rules';
-import { css } from 'js-beautify';
-import CSSLint from 'csslint';
-import 'codemirror/mode/css/css';
-import 'codemirror/addon/lint/lint';
-import 'codemirror/addon/lint/css-lint.js';
-import _ from 'lodash';
+import React, { useState, useEffect } from 'react'
+import { Controlled as CodeMirror } from 'react-codemirror2'
+import 'codemirror/addon/edit/matchbrackets'
+import 'codemirror/addon/edit/closebrackets'
+import 'codemirror/addon/comment/comment'
+import 'codemirror/addon/lint/lint.css'
+import 'codemirror/addon/search/match-highlighter'
+import parseCSS from 'css-rules'
+import { css } from 'js-beautify'
+import CSSLint from 'csslint'
+import 'codemirror/mode/css/css'
+import 'codemirror/addon/lint/lint'
+import 'codemirror/addon/lint/css-lint.js'
+import _ from 'lodash'
 
-if (typeof window !== `undefined`) {
-    window.CSSLint = CSSLint.CSSLint;
+if (typeof window !== 'undefined') {
+  window.CSSLint = CSSLint.CSSLint
 }
 
 const initialEditorOptions = {
-    value: `/* DELETE ME TO EXECUTE */
+  value: `/* DELETE ME TO EXECUTE */
 
 example1 {
   height: 5px;
 }`,
-    data: {},
-    editor: {},
-};
+  data: {},
+  editor: {}
+}
 
 const debouncedUpdateTree = _.debounce(
-    (setCssTree, parse, value, setEditorErrors, errors) => {
-        setCssTree(parse(value));
-        setEditorErrors(errors);
-    },
-    500
-);
+  (setCssTree, parse, value, setEditorErrors, errors) => {
+    setCssTree(parse(value))
+    setEditorErrors(errors)
+  },
+  500
+)
 
 const handleCssFileModifiers = _.debounce(
-    (mainFile) => {
-        console.log(mainFile, 'This is parsed mainFile for css');
-    },
-    2000
-);
+  (mainFile, finalCss) => {
+    var newCssArray = [];
+    var finalCssArray;
+    const convertedCss = mainFile.data
+    let cssArray = convertedCss.split('.hash-')
+    cssArray = cssArray.map(css => css.split('{'))    
+    const finalTailwindCss = JSON.parse(localStorage.getItem('finalTailwindCss'))
+    finalTailwindCss.map((val, index) => {
+      if(val.errors.length > 0) {
+        let checkKey = cssArray.findIndex(css => {
+          return 'hash-'+css[0].trim() == val.identifier.trim()
+        })
+        if(checkKey != -1){
+          if(newCssArray.indexOf(cssArray[checkKey]) === -1) {
+            newCssArray.push(cssArray[checkKey]);
+          }
+        }
+        finalCssArray = newCssArray.map(e => {
+          return '.hash-' + e[0] + '{' + e[1]
+        })
+      }
+    })
+    finalCss(finalCssArray.join('\n'))
+  },
+  2000
+)
 const handleHtmlFileModifiers = _.debounce(
-    (mainFile) => {
-        console.log(mainFile, 'This is parsed mainFile for html');
-        var convertedHtml = mainFile.data;
-        var finalTailwindCss = JSON.parse(localStorage.getItem('finalTailwindCss'));
-        finalTailwindCss.map((val, index) => {
-            if(val.errors.length > 0){
-                convertedHtml = convertedHtml.replaceAll(val.identifier, val.identifier+' '+val.value);
-            }else{
-                convertedHtml = convertedHtml.replaceAll(val.identifier, val.value);
-            }
-        });
-        console.log(mainFile, 'This is parsed mainFile for html');
-        console.log(convertedHtml, 'This is parsed mainFile for convertedHtml');
-    },
-    2500
-);
+  (mainFile, finalRes, allStyle, finalCss) => {
+    let convertedHtml = mainFile.data
+    const finalTailwindCss = JSON.parse(localStorage.getItem('finalTailwindCss'))
+    finalTailwindCss.map((val, index) => {
+      if (val.errors.length > 0) {
+        convertedHtml = convertedHtml.replaceAll(val.identifier, val.identifier + ' ' + val.value)
+      } else {
+        convertedHtml = convertedHtml.replaceAll(val.identifier, val.value)
+      }
+    })
+    localStorage.setItem('convertedHtml', convertedHtml)
+    finalRes(convertedHtml)
+    handleCssFileModifiers(allStyle, finalCss)
+  },
+  2500
+)
 
-const Editor = ({ setCssTree, setEditorErrors }) => {
-    const [editorState, setEditorState] = useState(initialEditorOptions);
-    const tidy = () => {
-        try {
-            setEditorState(() => {
-                return {
-                    ...editorState,
-                    value: css(editorState.value),
-                };
-            });
-        } catch (e) {
-            console.log('error formatting', e);
+const Editor = ({ setCssTree, setEditorErrors, finalRes, finalCss }) => {
+  const [editorState, setEditorState] = useState(initialEditorOptions)
+  const tidy = () => {
+    try {
+      setEditorState(() => {
+        return {
+          ...editorState,
+          value: css(editorState.value)
         }
-    };
-    const parse = (cssString) => {
-        try {
-            const parsedVal = parseCSS(cssString);
-            return parsedVal;
-        } catch (e) {
-            console.error('error parsing CSS', e);
-        }
-    };
+      })
+    } catch (e) {
+      console.log('error formatting', e)
+    }
+  }
+  const parse = (cssString) => {
+    try {
+      const parsedVal = parseCSS(cssString)
+      return parsedVal
+    } catch (e) {
+      console.error('error parsing CSS', e)
+    }
+  }
 
-    return (
+  return (
         <div className="relative h-full w-4/12">
             <div
                 className="absolute top-0 right-0 m-2 z-10 cursor-pointer text-gray-500 hover:text-gray-100"
                 onClick={() => {
-                    tidy();
+                  tidy()
                 }}
             >
                 <div className="font-bold" style={{ fontSize: '0.5rem' }}>
@@ -98,7 +119,7 @@ const Editor = ({ setCssTree, setEditorErrors }) => {
                     fill="none"
                     stroke="currentColor"
                     strokeLinecap="round"
-                    stroke-linejoin="round"
+                    strokeLinejoin="round"
                     strokeWidth="2"
                     viewBox="0 0 24 24"
                     className="w-6 h-4"
@@ -110,49 +131,49 @@ const Editor = ({ setCssTree, setEditorErrors }) => {
                 <CodeMirror
                     value={editorState.value}
                     options={{
-                        mode: 'css',
-                        theme: 'material',
-                        lineNumbers: true,
-                        matchBrackets: true,
-                        autoCloseBrackets: true,
-                        gutters: ['CodeMirror-lint-markers'],
-                        lint: true,
+                      mode: 'css',
+                      theme: 'material',
+                      lineNumbers: true,
+                      matchBrackets: true,
+                      autoCloseBrackets: true,
+                      gutters: ['CodeMirror-lint-markers'],
+                      lint: true
                     }}
                     id="my-codemirror-2"
                     onBeforeChange={(editor, data, value) => {
-                        setEditorState({ editor, data, value });
+                      setEditorState({ editor, data, value })
                     }}
                     editorDidMount={(editor, [next]) => {
-                        debouncedUpdateTree(
-                            setCssTree,
-                            parse,
-                            initialEditorOptions.value,
-                            setEditorErrors,
-                            editor.state.lint.marked.length > 0
-                        );
+                      debouncedUpdateTree(
+                        setCssTree,
+                        parse,
+                        initialEditorOptions.value,
+                        setEditorErrors,
+                        editor.state.lint.marked.length > 0
+                      )
                     }}
                     onChange={async (editor, data, value) => {
-                        const style = await fetch('http://localhost:3000/getStyle')
-                        const html = await fetch('http://localhost:3000/getHtml')
-                        const allStyle = await style.json();
-                        const allHtml = await html.json();
-                        debouncedUpdateTree(
-                            setCssTree,
-                            parse,
-                            allStyle.data,
-                            setEditorErrors,
-                            editor.state.lint.marked.length > 0
-                        );
-                        handleCssFileModifiers(allStyle);
-                        handleHtmlFileModifiers(allHtml);
-                        // setCssTree(parse(value));
-                        // setEditorErrors(editor.state.lint.marked.length > 0);
-                        // console.log(editor, data, parse(value));
+                      const style = await fetch('http://localhost:3000/getStyle')
+                      const html = await fetch('http://localhost:3000/getHtml')
+                      const allStyle = await style.json()
+                      const allHtml = await html.json()
+                      debouncedUpdateTree(
+                        setCssTree,
+                        parse,
+                        allStyle.data,
+                        setEditorErrors,
+                        editor.state.lint.marked.length > 0
+                      )
+                      // handleCssFileModifiers(allStyle, finalCss)
+                      handleHtmlFileModifiers(allHtml, finalRes, allStyle, finalCss)
+                      // setCssTree(parse(value));
+                      // setEditorErrors(editor.state.lint.marked.length > 0);
+                      // console.log(editor, data, parse(value));
                     }}
                 />
             )}
         </div>
-    );
-};
+  )
+}
 
-export default Editor;
+export default Editor
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,40 @@
-import React, { useState } from 'react';
-import SEO from '../components/seo';
-import Editor from '../components/editor';
-import '../style.css';
-import Settings from '../components/settings';
-import Output from '../components/output';
+import React, { useState } from 'react'
+import SEO from '../components/seo'
+import Editor from '../components/editor'
+import '../style.css'
+import Settings from '../components/settings'
+import Output from '../components/output'
 
 const IndexPage = () => {
-    const [cssTree, setCssTree] = useState([]);
-    const [editorErrors, setEditorErrors] = useState(false);
-    const [settings, setSettings] = useState({
-        remConversion: 16,
-        autoConvertSpacing: true,
-        autoConvertColor: true,
-    });
-    return (
+  const [cssTree, setCssTree] = useState([])
+  const [finalResHtml, setFinalResHtml] = useState('')
+  const [finalResCss, setFinalResCss] = useState('')
+  const [editorErrors, setEditorErrors] = useState(false)
+  const [settings, setSettings] = useState({
+    remConversion: 16,
+    autoConvertSpacing: true,
+    autoConvertColor: true
+  })
+  const finalRes = (val) => {
+    setFinalResHtml(val)
+  }
+  const finalCss = (val) => {
+    setFinalResCss(val)
+  }
+  return (
+    
         <div
             className="h-screen w-screen max-w-full overflow-hidden flex relative"
             style={{ minWidth: '812px' }}
         >
-            <Editor setCssTree={setCssTree} setEditorErrors={setEditorErrors} />
+            <Editor setCssTree={setCssTree} setEditorErrors={setEditorErrors} finalRes={finalRes} finalCss={finalCss}/>
             <SEO
                 title="Convert Css To Tailwind"
                 meta={[
-                    {
-                        name: 'google-site-verification',
-                        content: 'MiBwrqoOFZRpmJ4Ar52jHqGy91bRDEdXqFiUZS9pxB8',
-                    },
+                  {
+                    name: 'google-site-verification',
+                    content: 'MiBwrqoOFZRpmJ4Ar52jHqGy91bRDEdXqFiUZS9pxB8'
+                  }
                 ]}
             />
             <div className="flex flex-col h-full flex-grow relative">
@@ -34,27 +43,20 @@ const IndexPage = () => {
                     settings={settings}
                     editorErrors={editorErrors}
                 />
-                <a
-                    href="https://github.com/stevezease/tailwind-converter"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <svg
-                        viewBox="0 0 16 16"
-                        fill="currentColor"
-                        className="absolute w-5 h-5 hover:text-teal-600 transition duration-150 cursor-pointer"
-                        style={{ right: '40px', top: '13px' }}
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
-                        ></path>
-                    </svg>
-                </a>
-                <Settings setSettings={setSettings} settings={settings} />
+            </div>
+            <div className="flex flex-col h-full flex-grow relative" style={{fontSize:'10px', maxWidth: '40% !important'}}>
+                FINAL HTML
+                <div className="row" style={{border:'1px solid', marginBottom:'5px'}}>
+                    {finalResHtml}
+                </div>
+
+                FINAL CSS
+                <div className="row" style={{border:'1px solid'}}>
+                    {finalResCss}
+                </div>
             </div>
         </div>
-    );
-};
+  )
+}
 
-export default IndexPage;
+export default IndexPage
