3e4e90d3829ed1acb165ec8c346829939c5080b8
Abhishek Vishwakarma <52538014+abhigdrv@users.noreply.github.com>
2023-02-26 14:20:32 +0530

    fix: hanlde dynamic values

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\user\\Documents\\learning\\styleToTailwind\\tailwind-converter\\.cache\\dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\user\\Documents\\learning\\styleToTailwind\\tailwind-converter\\src\\pages\\404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\user\\Documents\\learning\\styleToTailwind\\tailwind-converter\\src\\pages\\index.js"))),
   "component---src-pages-page-2-js": hot(preferDefault(require("C:\\Users\\user\\Documents\\learning\\styleToTailwind\\tailwind-converter\\src\\pages\\page-2.js")))
